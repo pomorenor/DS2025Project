715ed9be3354025a4073c73586fadd11392cc886
JephT98 <jephersonbriant@gmail.com>
2025-07-15 21:09:05 -0500

    Agregando métodos útiles para la clase Student (setters, getters, toString y métodos para agregar deportes)

--- DeportesUnal/src/main/java/DataTypes/Student.java
+++ DeportesUnal/src/main/java/DataTypes/Student.java
@@ -4,6 +4,9 @@
  */
 package DataTypes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author pohlj
@@ -12,7 +15,81 @@ public class Student {
  
     //Attributes 
     
-    int id;
-    String name;
+    private int id;
+    private String name;
+    private List<String> sports;
+    private List<String> interest;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.sports = new ArrayList<>();
+        this.interest = new ArrayList<>();
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getSports() {
+        return sports;
+    }
+
+    public List<String> getInterest() {
+        return interest;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSports(List<String> sports) {
+        this.sports = sports;
+    }
+
+    public void setInterest(List<String> interest) {
+        this.interest = interest;
+    }
+
+    // Métodos para agregar deportes, evitando duplicados
+    public void addSport(String sport) {
+        if (!sports.contains(sport)) {
+            sports.add(sport);
+        }
+    }
+
+    public void addInterest(String sport) {
+        if (!interest.contains(sport)) {
+            interest.add(sport);
+        }
+    }
+
+    // Opcional: eliminar deporte
+    public void removeSport(String sport) {
+        sports.remove(sport);
+    }
+
+    public void removeInterest(String sport) {
+        interest.remove(sport);
+    }
+
+    // Para poder imprimir el estudiante
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               ", Nombre: " + name +
+               ", Practica: " + sports +
+               ", Le interesa: " + interest;
+    }
     
 }
