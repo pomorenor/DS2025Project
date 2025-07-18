264d6698ad2a62e61a38263919a2c5721b12f701
Julian F. Perez O. <julperezos@unal.edu.co>
2025-07-18 18:36:51 -0500

    Added constructors to Heap data structure

--- DeportesUnal/src/main/java/DataStructures/Heap.java
+++ DeportesUnal/src/main/java/DataStructures/Heap.java
@@ -1,4 +1,20 @@
 package DataStructures;
 
+import DataTypes.Student;
+
 public class Heap{
+    private final static int defaultCapacity = 8;
+    private Student[] students;
+    private int size;
+    private int max_size;
+
+    public Heap(int capacity){
+        students = new Student[capacity];
+        size = 0;
+        max_size = capacity;
+    }
+
+    public Heap(){
+        this(defaultCapacity);
+    }
 }
